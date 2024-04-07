80664edaedb0daf3ccca8781bb02eb0c41798d83
Nikhil Lalgudi Vaidyanathan <144494028+nikhil-lalgudi@users.noreply.github.com>
2024-04-07 14:24:44 -0400

    Add files via upload
    Possibly working scraper?

---
+++ Web Scraper.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import requests
+from bs4 import BeautifulSoup
+url = 'https://ar5iv.labs.arxiv.org/html/2304.01852'
+response = requests.get(url)
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text, 'html.parser')
+    print(soup.prettify())
+else:
+    print(f"Failed to retrieve the page. Status code: {response.status_code}")
+
