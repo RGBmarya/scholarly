9383e27f0962faab165d460b49bce6ba1f166a25
Mihir Arya <msarya@icloud.com>
2024-04-17 23:11:34 -0400

    test

--- scholarly-app/app/home/Search.tsx
+++ scholarly-app/app/home/Search.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
-import { Button, Input } from "@geist-ui/core"
+"use client"
 
-const Search: React.FC = () => {
+import React, { useState } from 'react';
+import { Button, Input } from "@geist-ui/core";
+import { getPaper } from "@/app/api/getPaper";
+
+export default function Search() {
+    const [doi, setDoi] = useState(""); // State to store the DOI input value
+    const getPaperWithDoi = getPaper.bind(doi)
     return (
         <>
             <div className="flex flex-col items-center space-y-2 text-center">
@@ -10,14 +15,12 @@ const Search: React.FC = () => {
                     Enter the DOI (Digital Object Identifier) of a research paper to find relevant articles and publications.
                 </p>
             </div>
-            <div className="flex flex-col gap-2 w-full max-w-3xl p-4 border rounded-lg border-gray-200 bg-white shadow-sm md:p-6">
+            <form className="flex flex-col gap-2 w-full max-w-3xl p-4 border rounded-lg border-gray-200 bg-white shadow-sm md:p-6">
                 <div className="grid gap-1">
-                    <Input label="DOI" id="doi" placeholder="10.1126/science.169.3946.635" width="100%" />
+                    <Input label="DOI" id="doi" placeholder="10.1126/science.169.3946.635" width="100%" value={doi} onChange={(e) => setDoi(e.target.value)} />
                 </div>
-                <Button>Search</Button>
-            </div>
+                <Button onClick={getPaperWithDoi}>Search</Button>
+            </form>
         </>
     );
-};
-
-export default Search;
+}
