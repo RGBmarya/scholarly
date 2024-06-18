fdd022216e7ddb1c621a01a935d0ea5d38cc1c26
Mihir Arya <msarya@icloud.com>
2024-06-18 13:50:58 -0700

    Added explain endpoint

---
+++ scholarly-app/app/api/explain/route.tsx
@@ -0,0 +1,22 @@
+import { NextRequest } from "next/server";
+import { createOpenAI } from "@ai-sdk/openai";
+import { getLLMResponse } from "@/lib/llm";
+export const dynamic = 'force-dynamic' // defaults to auto
+
+const openai = createOpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const model = openai("gpt-3.5-turbo")
+
+  
+export async function POST(request: NextRequest) {
+    try { 
+        const { currentSelection, namespace } = await request.json();
+        const response = await getLLMResponse(currentSelection, namespace);
+        return response;
+    }
+    catch (error) {
+        console.error(error)
+        throw new Error("Error: POST request for highlighted text explanation");
+    }
+}
